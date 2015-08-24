c4b1b31c365d33ea5136188edf0f872aa974ae41
thanh <thanhrossi.tt@gmail.com>
2015-08-24 18:10:32 +0700

    add file with data

---
+++ js/blog.js
@@ -0,0 +1,34 @@
+$(function() {
+
+    Parse.$ = jQuery;
+
+    // Replace this line with the one on your Quickstart Guide Page
+    Parse.initialize("bK6HuUZkBZCD5H0OybbyZ9JbAllTJwQFKVlNZbtr", "fhl97p5zPUHlaFQBoVwfFvvXHfYgvX9HoNOkPVn8");
+    var Blog = Parse.Object.extend("Blog");
+    var Blogs = Parse.Collection.extend({
+        model: Blog
+    });
+    var blogs = new Blogs();
+    blogs.fetch({
+        success: function(blogs) {
+            var blogsView = new BlogsView({
+                collection: blogs
+            });
+            blogsView.render();
+            $('.main-container').html(blogsView.el);
+        },
+        error: function(blogs, error) {
+            console.log(error);
+        }
+    });
+    var BlogsView = Parse.View.extend({
+        template: Handlebars.compile($('#blogs-tpl').html()),
+        render: function() {
+            var collection = {
+                blog: this.collection.toJSON()
+            };
+            this.$el.html(this.template(collection));
+        }
+    });
+
+});
